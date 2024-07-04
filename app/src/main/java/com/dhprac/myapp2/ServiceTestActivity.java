1071b566d65ee9eed4ad230aa11ea7f3fcdc33a1
dm029 <dh@beaunex.net>
2024-07-04 13:43:57 +0900

    edit code

--- app/src/main/java/com/dhprac/myapp2/ServiceTestActivity.java
+++ app/src/main/java/com/dhprac/myapp2/ServiceTestActivity.java
@@ -3,6 +3,7 @@ package com.dhprac.myapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,7 +32,15 @@ public class ServiceTestActivity extends AppCompatActivity {
         public void onClick(View v) {
             // 서비스 가동
             serviceIntent = new Intent(ServiceTestActivity.this, MyService.class);
-            startService(serviceIntent);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // 안드로이드 8.0 이상은 포그라운드로 운영
+                startForegroundService(serviceIntent);
+            } else  {
+                // 서비스 가동
+                startService(serviceIntent);
+            }
+
         }
     }
 
@@ -42,4 +51,5 @@ public class ServiceTestActivity extends AppCompatActivity {
             stopService(serviceIntent);
         }
     }
+
 }
--- app/src/main/java/com/dhprac/myapp2/service/MyService.java
+++ app/src/main/java/com/dhprac/myapp2/service/MyService.java
@@ -1,11 +1,23 @@
 package com.dhprac.myapp2.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
+import com.dhprac.myapp2.R;
+
 public class MyService extends Service {
+
+
+    static final String NOTIFICATION_CHANNEL_ID = "service";
+    static final String NOTIFICATION_CHANNEL_NAME = "service";
+    static final int MESSAGE_ID = 11;
     public MyService() {
     }
 
@@ -19,6 +31,18 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e("test", "서비스가 가동되었습니다.");
 
+        // 안드로이드 8.0 이상부터는 포그라운드 서비스로 해야한다.
+        addNotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME);
+
+        NotificationCompat.Builder builder = getNotificationBuilder(NOTIFICATION_CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setContentTitle("서비스 가동");
+        builder.setContentText("서비스 가동중입니다.");
+
+        Notification build = builder.build();
+        // 알림 메시지를 포그라운드 서비스를 위해 표시
+        startForeground(MESSAGE_ID, build);
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -26,5 +50,25 @@ public class MyService extends Service {
     public void onDestroy() {
         Log.e("test", "서비스가 종료되었습니다.");
         super.onDestroy();
+    }
+
+    private void addNotificationChannel(String id, String name) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationChannel channel = notificationManager.getNotificationChannel(id);
+
+            // 채널 값이 없다면, 객체 생성
+            if (channel == null) {
+                channel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_DEFAULT);
+
+                channel.enableVibration(true);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(String id) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, id);
+        return builder;
     }
 }
